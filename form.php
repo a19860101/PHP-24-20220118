acd3de03bf943021ee33120c44919fc8560c055f
Zac <a19860101@gmail.com>
2022-01-20 20:34:06 +0800

    20220120

---
+++ form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="response.php" method="post">
+        <label for="">姓名</label>
+        <input type="text" name="name">
+        <br>
+        <label for="">MAIL</label>
+        <input type="text" name="mail">
+        <br>
+        <label for="">電話</label>
+        <input type="text" name="phone">
+        <input type="submit" value="送出">
+    </form>
+</body>
+</html>
---
+++ response.php
@@ -0,0 +1,6 @@
+<?php
+    echo 'Response!!';
+
+    //超級全域變數 superglobal
+    var_dump($_POST);
+
