15284f3891fdf1cff78695001910c63a2f37a471
Zac <a19860101@gmail.com>
2022-01-20 21:12:35 +0800

    20220120

--- form.php
+++ form.php
@@ -7,7 +7,20 @@
     <title>Document</title>
 </head>
 <body>
+    <h2>POST</h2>
     <form action="response.php" method="post">
+        <label for="">姓名</label>
+        <input type="text" name="name">
+        <br>
+        <label for="">MAIL</label>
+        <input type="text" name="mail">
+        <br>
+        <label for="">電話</label>
+        <input type="text" name="phone">
+        <input type="submit" value="送出">
+    </form>
+    <h2>GET</h2>
+    <form action="response.php" method="get">
         <label for="">姓名</label>
         <input type="text" name="name">
         <br>
--- response.php
+++ response.php
@@ -1,11 +1,28 @@
 <?php
-    echo 'Response!!';
+    // echo 'Response!!';
 
     //超級全域變數 superglobal
-    var_dump($_POST);
-    echo $_POST['name'];
+    // var_dump($_POST);
+    // echo $_POST['name'];
+    // echo '<br>';
+    // echo $_POST['mail'];
+    // echo '<br>';
+    // echo $_POST['phone'];
+
+    // if(isset($_POST['name'])){
+    //     extract($_POST);
+    // }else{
+    //     extract($_GET);
+    // }
+    
+    print_r($_REQUEST);
+    extract($_REQUEST);
+
+    echo $name;
     echo '<br>';
-    echo $_POST['mail'];
+    echo $mail;
     echo '<br>';
-    echo $_POST['phone'];
+    echo $phone;
+
+    
 
