f2f20f25ff34d656fad273e75743f992f6231d82
Zac <a19860101@gmail.com>
2022-01-20 19:06:06 +0800

    20220120

--- array.php
+++ array.php
@@ -6,15 +6,26 @@
 
     // $a = array('Cat','Dog');
 
-    $a = ['HTML','CSS','Javascript'];
+    $a = ['HTML','CSS','Javascript','PHP','MySQL'];
     
 
-    print_r($a);
+    // print_r($a);
     // var_dump($a);
-    echo '<br>';
-    echo $a[0];
-    echo '<br>';
-    echo $a[1];
-    echo '<br>';
-    echo $a[2];
-    echo '<br>';
+    // echo '<br>';
+    // echo $a[0];
+    // echo '<br>';
+    // echo $a[1];
+    // echo '<br>';
+    // echo $a[2];
+    // echo '<br>';
+    
+    //列舉 迭代
+    // for($i=0;$i<3;$i++){
+    //     echo $a[$i];
+    //     echo '<br>';
+    // }
+
+    foreach($a as $item){
+        echo $item;
+        echo '<br>';
+    }
