0582b0659c407724f878efb02d5fb64226cd3e7e
Zac <a19860101@gmail.com>
2022-01-20 21:54:29 +0800

    20220120

--- student/store.php
+++ student/store.php
@@ -1,17 +1,24 @@
 <?php
     extract($_REQUEST);
 
-    echo $name;
-    echo '<br>';
-    echo $phone;
-    echo '<br>';
-    echo $email;
-    echo '<br>';
-    echo $gender;
-    echo '<br>';
-    echo $edu;
-    echo '<br>';
-    // print_r($skill);
-    echo implode(',',$skill);
-    echo '<br>';
-    echo $comment;
+    // echo $name;
+    // echo '<br>';
+    // echo $phone;
+    // echo '<br>';
+    // echo $email;
+    // echo '<br>';
+    // echo $gender;
+    // echo '<br>';
+    // echo $edu;
+    // echo '<br>';
+    // // print_r($skill);
+    // echo implode(',',$skill);
+    // echo '<br>';
+    // echo $comment;
+
+    // trim()
+    // var_dump($name);
+    // var_dump(trim($name));//前後空白
+    // var_dump(stripslashes($name));//反斜線
+    // var_dump(htmlspecialchars($name));//特殊字元 < > " ' &
+    echo htmlspecialchars($name);
