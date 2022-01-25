bc08a2d1d17279a3a7bd0c7ce2ac04a8bdd0b4d3
Zac <a19860101@gmail.com>
2022-01-25 21:49:36 +0800

    20220125

--- student/index.php
+++ student/index.php
@@ -6,11 +6,22 @@
 
     $sql = 'SELECT * FROM students';
     $result = mysqli_query($db,$sql);
-    while($data = mysqli_fetch_assoc($result)){
-        // print_r($data);
-        echo $data['email'];
-    }
+    
+    // 方法一
+    // $datas = [];
+    // while($data = mysqli_fetch_assoc($result)){
+    //     $datas[] = $data;
+    // }
+    // foreach($datas as $data){
+    //     echo $data['name'];
+    // }
 
+    // 方法二
+    $datas = mysqli_fetch_all($result);
+    print_r($datas);
+    foreach($datas as $data){
+        print_r($data);
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
