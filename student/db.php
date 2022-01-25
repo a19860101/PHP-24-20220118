7143859ee61941b92f74580358e19f71152680c7
Zac <a19860101@gmail.com>
2022-01-25 21:31:57 +0800

    20220125

--- student/db.php
+++ student/db.php
@@ -1,9 +1,11 @@
 <?php
     $db_host = 'localhost';
-    $db_user = 'amin';
+    $db_user = 'admin';
     $db_pw = 'admin';
     $db_name = 'php-24-20220118';
 
 
     // $db = @mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('資料庫連線錯誤，請洽管理員!');
     $db = mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('資料庫連線錯誤，請洽管理員!');
+
+    mysqli_query($db,'SET NAMES utf8mb4');
