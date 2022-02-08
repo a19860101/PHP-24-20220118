84c1c750ac173219897df87ab0491e97888be2de
Zac <a19860101@gmail.com>
2022-02-08 20:08:23 +0800

    20220208

--- student_pdo/pdo.php
+++ student_pdo/pdo.php
@@ -10,4 +10,32 @@
     // $dsn = "mysql:host=localhost;dbname=php-24-20220118;charset=utf8mb4";
     //data source name
 
-    $pdo = new PDO($dsn,$db_user,$db_pw);
+    try {
+        $pdo = new PDO($dsn,$db_user,$db_pw);
+        
+        // $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_SILENT);
+        //不主動報錯(預設)
+        
+        // $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
+        // 主動報錯
+        
+        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        // 主動例外
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+
+
+    try {
+        $sql = 'SELECT * FROM student WHERE id = 1';
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $datas = $stmt->fetchAll();
+        var_dump($datas);
+    }catch(PDOException $e){
+        echo $e->getMessage();
+        // var_dump($e);
+    }
+
+    
