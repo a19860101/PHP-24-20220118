c8be17e581372ef26a999b011bb28f37f7ea2bc7
Zac <a19860101@gmail.com>
2022-02-10 20:11:27 +0800

    20220210

--- filesystem_pdo/upload.php
+++ filesystem_pdo/upload.php
@@ -1,49 +1,48 @@
 <?php
-    // var_dump($_FILES['img']);
-    // echo $_FILES['img']['name'];
-    // echo $_FILES['img']['tmp_name'];
-    // echo $_FILES['img']['type'];
-    // echo $_FILES['img']['error'];
-    // echo $_FILES['img']['size'];
 
-    extract($_FILES['img']);
-    // echo $name;
-    // echo $tmp_name;
-    // echo $type;
-    // echo $error;
-    // echo $size;
-    
-    // 如果資料夾不存在就建立
-    if(!is_dir('images')){
-        mkdir('images');
-    }
+    function imgUpload($files){
+        extract($files);
+        
+        if(!is_dir('images')){
+            mkdir('images');
+        }
 
-    //檔名
-    $img_name = md5(time());
-    //副檔名
-    $ext = pathinfo($name,PATHINFO_EXTENSION);
-    //完整檔名
-    $fullname = $img_name.'.'.$ext;
+        $img = [];
 
-    $target = 'images/'.$fullname;
+        //檔名
+        $img_name = md5(time());
+        //副檔名
+        $ext = pathinfo($name,PATHINFO_EXTENSION);
+        //完整檔名
+        $fullname = $img_name.'.'.$ext;
 
-    //
-    if($ext != 'jpg' && $ext != 'jpeg' && $ext != 'png' && $ext != 'gif' && $ext != 'webp'){
-        echo '<script>alert("請上傳正確的圖片格式！")</script>';
-        header('refresh:0;url=form.php');
-        return;
-    }
+        $target = 'images/'.$fullname;
 
-
-    if($error == 0){
-        if(move_uploaded_file($tmp_name,$target)){
-            echo '<script>alert("上傳成功")</script>';
+        //
+        if($ext != 'jpg' && $ext != 'jpeg' && $ext != 'png' && $ext != 'gif' && $ext != 'webp'){
+            echo '<script>alert("請上傳正確的圖片格式！")</script>';
             header('refresh:0;url=form.php');
+            return;
+        }
+
+
+        if($error == 0){
+            if(move_uploaded_file($tmp_name,$target)){
+                // echo '<script>alert("上傳成功")</script>';
+                // header('refresh:0;url=form.php');
+                $img['path'] = $fullname;
+                $img['name'] = $name;
+                return $img;
+            }else{
+                echo '<script>alert("上傳失敗")</script>';
+                header('refresh:0;url=form.php');
+            }
         }else{
-            echo '<script>alert("上傳失敗")</script>';
+            echo '<script>alert("上傳錯誤")</script>';
             header('refresh:0;url=form.php');
         }
-    }else{
-        echo '<script>alert("上傳錯誤")</script>';
-        header('refresh:0;url=form.php');
     }
+
+    $img = imgUpload($_FILES['img']);
+
+    var_dump($img);
