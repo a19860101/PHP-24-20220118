8e0af3bdcbe99e6636bcc9f81ff31352d8a6a59d
Zac <a19860101@gmail.com>
2022-02-15 19:51:10 +0800

    20220215

--- member/store.php
+++ member/store.php
@@ -1,6 +1,19 @@
 <?php
     include('pdo.php');
     extract($_REQUEST);
+
+    //檢查是否有相同的帳號(email)
+    $sql_check = 'SELECT * FROM users WHERE email = ?';
+    $stmt_check = $pdo->prepare($sql_check);
+    $stmt_check->execute([$email]);
+
+    if($stmt_check->rowCount() > 0){
+        echo '<script>alert("此Email已被註冊，請重新輸入!")</script>';
+        header('refresh:0;url=register.php');
+        return;
+    }
+    
+    // 密碼加密
     $pw = sha1(md5($pw));
     $sql = 'INSERT INTO users(name,email,pw,created_at)VALUES(?,?,?,?)';
 
