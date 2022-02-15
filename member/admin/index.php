56a499ee3161d6ea4d093b5a6b328878d58575cb
Zac <a19860101@gmail.com>
2022-02-15 21:08:01 +0800

    20220215

--- member/admin/index.php
+++ member/admin/index.php
@@ -1,5 +1,13 @@
 <?php
+    include('../pdo.php');
     session_start();
+    $sql = 'SELECT * FROM users ORDER BY created_at DESC';
+    try {
+
+        $users = $pdo->query($sql)->fetchAll();
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,6 +16,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        table,td,th {
+            border: 1px solid #aaa;
+            border-collapse: collapse;
+            padding: 10px;
+        }
+
+    </style>
 </head>
 <body>
     <nav>
@@ -21,5 +37,31 @@
         <?php } ?>
     </nav>
     <h2>管理後台</h2>
+    <table>
+        <tr>
+            <th>#</th>
+            <th>名稱</th>
+            <th>E-mail</th>
+            <th>角色</th>
+            <th>註冊時間</th>
+            <th>動作</th>
+        </tr>
+        <?php foreach($users as $user){ ?>
+        <tr>
+            <td><?php echo $user['id'];?></td>
+            <td><?php echo $user['name'];?></td>
+            <td><?php echo $user['email'];?></td>
+            <td>
+                <?php echo $user['role']==0?'管理員':'一般會員';?>
+            </td>
+            <td><?php echo $user['created_at'];?></td>
+            <td>
+                <form action="" method="post">
+                    <input type="submit" value="切換權限">
+                </form>
+            </td>
+        </tr>
+        <?php } ?>
+    </table>
 </body>
 </html>
