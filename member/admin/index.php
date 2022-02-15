aac6827776b006255116be548e0b222ecad8ea4e
Zac <a19860101@gmail.com>
2022-02-15 20:33:35 +0800

    20220215

---
+++ member/admin/index.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <?php if(!isset($_SESSION['AUTH'])){ ?>
+        <a href="../login.php">會員登入</a>
+        <a href="../register.php">會員註冊</a>
+        <?php } ?>
+        <?php if(isset($_SESSION['AUTH'])){ ?>
+        <span><?php echo $_SESSION['AUTH']['name'];?></span>
+        <a href="../logout.php">登出</a>
+        <?php } ?>
+    </nav>
+    <h2>管理後台</h2>
+</body>
+</html>
