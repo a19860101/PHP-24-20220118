56623b5902e4be0c80d345424a006513d280c35f
Zac <a19860101@gmail.com>
2022-02-17 20:32:21 +0800

    20220217

--- class/basic2.php
+++ class/basic2.php
@@ -1,18 +1,39 @@
 <?php
     class User {
-        public $name = 'John';
+        public $publicName = 'public';
+        private $privateName = 'private';
+        protected $protectedName = 'protected';
         public $mail = 'john@gmail.com';
 
         public function go(){
             return $this->name.' Go';
         }
+        public function test(){
+            // return $this->privateName;
+            return $this->protectedName;
+        }
     }
 
     class AdminUser extends User{
-
+        public function test2(){
+            return $this->protectedName;
+        }
     }
 
-    $x = new AdminUser;
-    $x->name = 'Mary';
-    var_dump($x);
+    $x = new User;
+    $y = new AdminUser;
+    // $x->name = 'Mary';
+    // var_dump($x);
+
+    // echo $x->publicName;
+    // echo $y->publicName;
+    // echo $x->privateName;
+    // echo $y->privateName;
+    // echo $x->test();
+    // echo $y->test();
+    // echo $x->protectedName;
+    // echo $y->protectedName;
+
+    echo $y->test2();
+    // var_dump($y);
 
