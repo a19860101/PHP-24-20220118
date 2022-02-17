5f72870d71dd34557c4955e7caa4d5570ac943f3
Zac <a19860101@gmail.com>
2022-02-17 19:51:49 +0800

    20220217

--- class/basic.php
+++ class/basic.php
@@ -1,19 +1,35 @@
 <?php
-    class Test {
-        //物件屬性(變數)
-        public $x = 10;
-        public $y = 99;
-        //物件方法(函式)
-        public function asdf(){
-            return 'Hello';
+    class User {
+        public $name;
+        public $nickname;
+        public $tacticalAbility;
+        public $passiveAbility;
+        public $ultimateAbility;
+
+        function run(){
+            return 'run';
         }
+        function jump(){
+            return 'jump';
+        }
+        function attack(){
+            return 'attack';
+        }
+
     }
 
+    $ash = new User;
+    $ash->name = '艾許';
+    $ash->nickname = '敏銳煽動者';
+    $ash->tacticalAbility = '電弧絆索';
+    $ash->passiveAbility = '攻防戰';
+    $ash->ultimateAbility = '相移裂縫';
+    var_dump($ash);
 
-    $a = new Test;
-
-    var_dump($a);
-
-    $b = new Test;
-
-    var_dump($b);
+    $fuse = new User;
+    $fuse->name = '轟哥';
+    $fuse->nickname = '浮誇的爆炸專家';
+    $fuse->tacticalAbility = '榴彈槍';
+    $fuse->passiveAbility = '榴彈兵';
+    $fuse->ultimateAbility = 'Motherlode';
+    var_dump($fuse);
