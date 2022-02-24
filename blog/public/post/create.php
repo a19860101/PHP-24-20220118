be3d057d793270fae7faadd9c7d2c70f432c087c
Zac <a19860101@gmail.com>
2022-02-24 19:34:12 +0800

    20220224

---
+++ blog/public/post/create.php
@@ -0,0 +1,29 @@
+<?php include('../template/header.php'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-xl-6 col-sm-8">
+            <h2>建立文章</h2>
+            <hr>
+            <form action="">
+                <div class="mb-3">
+                    <label for="title" class="form-label">文章標題</label>
+                    <input type="text" name="title" id="title" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="content" class="form-label">文章內容</label>
+                    <textarea name="content" id="content" cols="30" rows="10" class="form-control"></textarea>
+                </div>
+                <div class="mb-3">
+                    <label for="category_id">文章分類</label>
+                    <select name="category_id" id="category_id" class="form-select">
+                        <option value="1">生活</option>
+                        <option value="2">健康</option>
+                        <option value="3">科技</option>
+                    </select>
+                </div>
+                <input type="submit" class="btn btn-primary" value="建立文章">
+            </form>
+        </div>
+    </div>
+</div>
+<?php include('../template/footer.php'); ?>
--- blog/public/template/header.php
+++ blog/public/template/header.php
@@ -1,33 +1,31 @@
+<?php
+    $webroot = 'http://localhost/PHP-24-20220118/blog/public/';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Blog</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-3">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Blog</a>
+        <a class="navbar-brand" href="<?php echo $webroot;?>">Blog</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="<?php echo $webroot;?>">首頁</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="<?php echo $webroot;?>post/create.php">建立文章</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                </li>
+                
             </ul>
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
