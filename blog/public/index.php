cafffb593b6238fb9a617646039f6d0bf3c4c3bd
Zac <a19860101@gmail.com>
2022-02-24 20:15:01 +0800

    20220224

--- blog/public/index.php
+++ blog/public/index.php
@@ -1,7 +1,24 @@
 <?php
     require_once('../vendor/autoload.php');
+    $posts = Gjun\Blog\Controller\Post::index();
 ?>
 
 <?php include('template/header.php'); ?>
+<div class="container">
+    <div class="row">
+        <?php foreach($posts as $post){ ?>
+        <div class="col-10">
+            <h2><?php echo $post['title'];?></h2>
+            <div>
+                <?php echo $post['content'];?>
+            </div>
+            <div>
+                最後更新時間 <?php echo $post['updated_at']; ?>
+            </div>
+            <hr>
+        </div>
+        <?php } ?>
+    </div>
+</div>
 <?php include('template/footer.php'); ?>
 
--- blog/src/Controller/Post.php
+++ blog/src/Controller/Post.php
@@ -3,6 +3,11 @@
     use Gjun\Blog\Config\DB;
 
     class Post extends DB {
+        function index(){
+            $sql = "SELECT * FROM posts ORDER BY id DESC";
+            $datas = DB::connect()->query($sql)->fetchAll();
+            return $datas;
+        }
         static function store($request){
             extract($request);
             $sql = "INSERT INTO posts(title,content,category_id,user_id,created_at,updated_at)VALUES(?,?,?,?,?,?)";
