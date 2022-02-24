2788a07f87ff1b5c0ba0f7ccfe663d20fa101b27
Zac <a19860101@gmail.com>
2022-02-24 21:57:31 +0800

    20220224

--- blog/public/post/create.php
+++ blog/public/post/create.php
@@ -26,4 +26,12 @@
         </div>
     </div>
 </div>
+<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
+    tinymce.init({
+        selector: '#content',
+        height: 600,
+        language: 'zh_TW'
+    });
+</script>
 <?php include('../template/footer.php'); ?>
