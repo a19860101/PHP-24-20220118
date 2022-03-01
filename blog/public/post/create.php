b48a2278b0f26a92d612cc7eb294725b418f5ed0
Zac <a19860101@gmail.com>
2022-03-01 20:06:00 +0800

    20220301

--- blog/public/post/create.php
+++ blog/public/post/create.php
@@ -1,3 +1,8 @@
+<?php
+    require_once('../../vendor/autoload.php');
+    use Gjun\Blog\Controller\Category;
+    $categories = Category::index();
+?>
 <?php include('../template/header.php'); ?>
 <div class="container">
     <div class="row justify-content-center">
@@ -10,17 +15,18 @@
                     <input type="text" name="title" id="title" class="form-control">
                 </div>
                 <div class="mb-3">
+                    <label for="category_id">文章分類</label>
+                    <select name="category_id" id="category_id" class="form-select">
+                        <?php foreach($categories as $category){ ?>
+                            <option value="<?php echo $category['id'];?>"><?php echo $category['title'];?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+                <div class="mb-3">
                     <label for="content" class="form-label">文章內容</label>
                     <textarea name="content" id="content" cols="30" rows="10" class="form-control"></textarea>
                 </div>
-                <div class="mb-3">
-                    <label for="category_id">文章分類</label>
-                    <select name="category_id" id="category_id" class="form-select">
-                        <option value="1">生活</option>
-                        <option value="2">健康</option>
-                        <option value="3">科技</option>
-                    </select>
-                </div>
+                
                 <input type="submit" class="btn btn-primary" value="建立文章">
             </form>
         </div>
